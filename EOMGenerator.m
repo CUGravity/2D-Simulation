4863f5268ee8b8137cc91e45e5ed066b13648a82
dtk42 <dtk42@CIT-ORANGE-06.cornell.edu>
2016-10-20 21:27:53 -0400

    Completed EOM Generator, needs debugging

--- EOMGenerator.m
+++ EOMGenerator.m
@@ -1,5 +1,5 @@
-function EOMGenerator
-
+function angleDD = EOMGenerator
+tic;
 %% Constants
 d_width = 0.1;
 d_G2T2 = 0.025;
@@ -16,7 +16,6 @@ I_G1 = m1*((d_G1T13+d_G1T13)^2+d_width^2)/12;
 I_G2 = m2*((2*d_G2T2)^2+d_width^2)/12;
 I_G3 = m3*((2*d_G3T3)^2+d_width^2)/12;
 
-
 %% Symbolic variables
 syms th1 th1d th1dd real;
 syms th2 th2d th2dd real;
@@ -24,6 +23,7 @@ syms th3 th3d th3dd real;
 syms phi12 phi12d phi12dd real; % measured from G1
 syms phi13 phi13d phi13dd real;
 syms dis_G1G2 disd_G1G2 disdd_G1G2 dis_G1G3 disd_G1G3 disdd_G1G3 real;
+syms coil1 coil2 coil3 real;
 
 %% Basis vectors
 i = [1 0 0]; j = [0 1 0]; k = [0 0 1];
@@ -78,25 +78,30 @@ a_G3G1 = (disdd_G1G3 - dis_G1G3*phi13d^2)*er_G1G3 + ...
 a_G3F = a_G3G1 + a_G1F;
 
 %% Spring Forces
-F_T12T2 = ks*(r_T12T2 - lo12)*hvs(r_T12T2 , lo12);
+F_T12T2 = ks*r_T12T2*(norm(r_T12T2) - lo12)*hvs(norm(r_T12T2) , lo12);
 F_T2T12 = -F_T12T2;
-F_T13T3 = ks*(r_T13T3 - lo13)*hvs(r_T13T3 , lo13);
+F_T13T3 = ks*r_T13T3*(norm(r_T13T3) - lo13)*hvs(norm(r_T13T3) , lo13);
 F_T3T13 = -F_T13T3;
 
+%% Torquers 
+tau_1 = coil1*k;
+tau_2 = coil2*k;
+tau_3 = coil3*k;
+
 %% AMB of System About G1
-% M_G1 = [0 0 0];
-% 
-% H_1G1 = cross( r_G1G1 , m1*a_G1F ) + I_G1*th1d*k;
-% H_2G1 = cross( r_G2G1 , m2*a_G2F ) + I_G2*th2d*k;
-% H_3G1 = cross( r_G3G1 , m3*a_G3F ) + I_G3*th3d*k;
-% H_G1 = H_1G1 + H_2G1 + H_3G1;
-% 
-% eqn1 = M_G1 == H_G1;
+M_G1 = tau_1 + tau_2 + tau_3;
+
+H_1G1 = cross( r_G1G1 , m1*a_G1F ) + I_G1*th1d*k;
+H_2G1 = cross( r_G2G1 , m2*a_G2F ) + I_G2*phi12d*k;
+H_3G1 = cross( r_G3G1 , m3*a_G3F ) + I_G3*phi13d*k;
+H_G1 = H_1G1 + H_2G1 + H_3G1;
+
+eqn1 = M_G1 == H_G1;
 
 %% AMB of 1 About G1
 M_T12G1 = cross( r_T12G1 , F_T12T2 );
 M_T13G1 = cross( r_T13G1 , F_T13T3 );
-M_G1 = M_T12G1 + M_T13G1;
+M_G1 = M_T12G1 + M_T13G1 + tau_1;
 
 H_G1G1 = cross( r_G1G1 , m1*a_G1F ) + I_G1*th1dd*k;
 H_G1 = H_G1G1;
@@ -105,7 +110,7 @@ eqn2 = M_G1 == H_G1;
 
 %% AMB of 2 About G2
 M_T2G2 = cross( r_T2G2 , F_T2T12 );
-M_G2 = M_T2G2;
+M_G2 = M_T2G2 + tau_2;
 
 H_G2G2 = cross( r_G2G2 , m2*a_G2F ) + I_G2*th2dd*k;
 H_G2 = H_G2G2;
@@ -114,20 +119,42 @@ eqn3 = M_G2 == H_G2;
 
 %% AMB of 3 About G3
 M_T3G3 = cross( r_T3G3 , F_T3T13 );
-M_G3 = M_T3G3;
+M_G3 = M_T3G3 + tau_3;
 
 H_G3G3 = cross( r_G3G3 , m3*a_G3F ) + I_G3*th3dd*k;
 H_G3 = H_G3G3;
 
 eqn4 = M_G3 == H_G3;
 
+%% LMB about 1
+F_t1 = F_T12T2 + F_T13T3;
+eqn5 = F_t1 == m1*a_G1F;
+
+%% LMB about 2
+F_t2 = F_T2T12;
+eqn6 = F_t2 == m2*a_G2F;
+
+%% LMB about 3
+F_t3 = F_T3T13;
+eqn7 = F_t3 == m3*a_G3F;
+
+%% Solve EOM equations
+
+equations = [ eqn1(3) eqn2(3) eqn3(3) eqn4(3) eqn5(1) eqn5(2) eqn6(1) ...
+    eqn6(2) eqn7(1) eqn7(2)];
+variables = [ th1dd th2dd th3dd phi12dd phi13dd disdd_G1G2 disdd_G1G3 ];
+
+angleDD = solve( equations , variables );
+
+matlabFunction(eqn1,'File','test');
+
+disp(['Script took ',num2str(toc),' seconds']);
 end
 
 function Heaviside = hvs(r,lo)
 % negative heaviside actually
-x = norm(r) - lo;
-khv = 10000;
+x = r - lo;
+khv = 1000;
 Heaviside = 1/(1 + exp(-2*khv*x));
-
 end
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 2D-Simulation
+
+Run EOM Generator to update the EOM files used in Spinning2D.
+
+Run Spinning2D to propagate the system.
+
+Use those outputs to run Spinning2DAnimate to produce the animations at a 
+given x-speed or to run Spinning2DPlot to generate plots of data versus 
+time.
